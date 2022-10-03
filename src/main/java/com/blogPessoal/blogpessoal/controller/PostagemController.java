8e634805a48edc0d5507aca26e888e6a61b5f7ed
carolmatosss <carolyne_nascimento@hotmail.com>
2022-10-03 15:14:06 -0300

    Atualização com PUT,POST E DELETE

--- src/main/java/com/blogPessoal/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/blogPessoal/blogpessoal/controller/PostagemController.java
@@ -1,28 +1,82 @@
 package com.blogPessoal.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.blogPessoal.blogpessoal.model.Postagem;
 import com.blogPessoal.blogpessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*") //Para que o FrontEnd consuma a API
+@CrossOrigin(origins = "*", allowedHeaders = "*") //Para que o FrontEnd consuma a API. Pra não limitar a porta, para liberar as rotas para outras plataformas. 
 public class PostagemController {
-	
+
 	@Autowired //A Injeção de Dependência define quais Classes serão instanciadas e em quais lugares serãoInjetadas quando houver necessidade.
 	private PostagemRepository postagemRepository;
-
+	//Traz o repositório que está extendo os método do JPA
 	@GetMapping
 	public ResponseEntity<List<Postagem>>getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+
+	@GetMapping ("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){ //Requisição na tabela o caminho é o Id
+		return postagemRepository.findById(id).map(resposta->ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	//Aqui é para quando tivermos uma resposta é para vir um ok
+	//-> Lambda acessa o método de forma resumida, uma função anonima que não cria necesidade de criar toda a lógica, resume para nos
+	//O else nos trás a resposta caso não seja encontrado a resposta que é um Http Status de não encontrado
+
+	@GetMapping ("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
+		//Método post para realizar uma postagem
+	}
+
+	@PutMapping
+	public ResponseEntity <Postagem> put (@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem) ))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	//Método put semelhante ao método post, a diferenca é que id precisa existir
+	
+	
+@ResponseStatus
+@DeleteMapping("/{id}")
+public void delete (@PathVariable Long id) {
+	Optional <Postagem> postagem = postagemRepository.findById(id);
+	
+	if (postagem.isEmpty())
+		throw new ResponseStatusException (HttpStatus.NOT_FOUND);
+	
+postagemRepository.deleteById(id);
+	
+	//Metodo delete deletando tudo a partir da inserção do Id no Endpoint
+	
+}
 
 }
--- src/main/java/com/blogPessoal/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/blogPessoal/blogpessoal/repository/PostagemRepository.java
@@ -1,13 +1,20 @@
 package com.blogPessoal.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.blogPessoal.blogpessoal.model.Postagem;
 
 @Repository //Classe que define os padrões de interação com o banco de dados, através das linguagens do mysql
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-//Aqui estamos mapeando para o banco de dados, onde o Long se refere a nossa Primary Key
-	
+	//Aqui estamos mapeando para o banco de dados, onde o Long se refere a nossa Primary Key
+
 	// Classe abstrata composta por métodos que serão implementados em uma classe
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	//Metodo public que trata uma lista de postagem e só serão buscados as infos do titulo. Isso está fora do JPA, por isso a lógica
+	//cria-se aqui
 }
