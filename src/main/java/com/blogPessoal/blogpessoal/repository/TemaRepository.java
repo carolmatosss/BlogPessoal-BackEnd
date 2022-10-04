2094306b8e38880e7c771732097352a7197e2f8a
carolmatosss <carolyne_nascimento@hotmail.com>
2022-10-04 14:03:35 -0300

    Arrumando Repository

--- src/main/java/com/blogPessoal/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/blogPessoal/blogpessoal/repository/TemaRepository.java
@@ -3,11 +3,12 @@ package com.blogPessoal.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.blogPessoal.blogpessoal.model.Tema;
 
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 
 	
-	public List <Tema> findAllByDescricaoContainingIgnoreCase (String Descricao);
+	public List <Tema> findAllByDescricaoContainingIgnoreCase (@Param("descricao")String Descricao);
 }
