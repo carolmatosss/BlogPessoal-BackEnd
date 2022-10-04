b5a85feb673c4a20a7abb20ff0af8ca341fe4d60
carolmatosss <carolyne_nascimento@hotmail.com>
2022-10-04 12:40:21 -0300

    Adicionando Temas

--- src/main/java/com/blogPessoal/blogpessoal/model/Postagem.java
+++ src/main/java/com/blogPessoal/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity //Entidade que transforma em tabela no banco
 @Table(name="tb_postagens") //Para dar nome na tabela
@@ -33,6 +36,20 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDate data;
+	
+	@ManyToOne //Configurando que a tabela postagem é de muitos para um
+	@JsonIgnoreProperties("postagem") //Para que não fique em um looping infinito entre tema e postagem, pois a relação no spring é bidirecional
+	private Tema tema;
+	
+	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
---
+++ src/main/java/com/blogPessoal/blogpessoal/model/Tema.java
@@ -0,0 +1,60 @@
+package com.blogPessoal.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascade =CascadeType.REMOVE)// tirar o ALL e por o REMOVE
+    //Essa anotação é feita para caso eu apague um tema, também seja apagado as postagens cadstradas, pois caso eu procure uma postagem sem tema, dará erro no meu banco de dados
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;  //A postagem é em lista, porque são muitas postagens
+
+	
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
