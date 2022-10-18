466c78419af5a7ebcf3f7ef3bbeedc519241e893
carolmatosss <carolyne_nascimento@hotmail.com>
2022-10-18 14:59:47 -0300

    Atualizando a camada de testes

--- src/main/java/com/blogPessoal/blogpessoal/model/Usuario.java
+++ src/main/java/com/blogPessoal/blogpessoal/model/Usuario.java
@@ -1,13 +1,19 @@
 package com.blogPessoal.blogpessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "usuario")
@@ -20,7 +26,7 @@ public class Usuario {
 	@NotBlank (message = "obrigatorio")
 	@Size(min=2 , max=20)
 	private String nome;
-    
+
 	private String foto; 
 
 
@@ -31,6 +37,26 @@ public class Usuario {
 	@NotBlank (message = "Ela é obrigatória")
 	@Size (min=8, message= "A senha deve ter no mínimo 8 caracteres")
 	private String senha;
+
+	@OneToMany (mappedBy= "usuario" , cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties  ("usuario")
+	public List<Postagem>  postagem;
+
+
+
+
+	public Usuario(Long id, String nome, String foto, String usuario, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.foto = foto;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+
+
+	public Usuario() {
+	}
+
 
 	public Long getId() {
 		return id;
@@ -70,6 +96,13 @@ public class Usuario {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 
 
